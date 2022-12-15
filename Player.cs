b2a018489e2c1b1ee9b61848a23c59cea824e06e
MaxencePechoux <maxencepechoux@gmail.com>
2022-12-15 07:42:48 -0500

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,490 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Player
+{
+    static void Main(string[] args)
+    {
+        GameInstance.Instance.Start();
+    }
+}
+
+public static class Constants
+{
+    public const int SCRAPS_TO_BUILD = 10;
+    public const int IDEAL_FACTORIES = 5;
+    public const int MIN_SCRAPS_TO_BUILD = 10;
+    public const int MIN_SCRAPS_TO_SPAWN = 10;
+    public const int NEUTRAL = -1;
+    public const int ME = 1;
+    public const int ENEMY = 0;
+    public const string MOVE = "MOVE";
+    public const string BUILD = "BUILD";
+    public const string SPAWN = "SPAWN";
+    public const string WAIT = "WAIT";
+    public const string MESSAGE = "MESSAGE";
+}
+
+public enum CampPosition
+{
+    RIGHT = -1,
+    INIT = 0,
+    LEFT = 1
+}
+
+public enum UnitTeam
+{
+    ATTACK,
+    DEFENSE,
+    BASE
+}
+
+public sealed class GameInstance
+{
+    private static GameInstance instance;
+    Logger logger = Logger.Instance;
+
+    private GameInstance()
+    {
+    }
+
+    public static GameInstance Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameInstance();
+            }
+            return instance;
+        }
+    }
+
+    public Tile[,] Map { get; private set; }
+
+    public int MapWidth { get; private set; }
+    public int MapHeight { get; private set; }
+    public CampPosition Direction { get; private set; }
+    public List<Tile> MyUnitsTiles { get; private set; }
+    public TeamManager TeamManager { get; private set; }
+
+
+    public void Start()
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        MapWidth = int.Parse(inputs[0]);
+        MapHeight = int.Parse(inputs[1]);
+        Direction = CampPosition.INIT;
+
+        // game loop
+        while (true)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int myMatter = int.Parse(inputs[0]);
+            int oppMatter = int.Parse(inputs[1]);
+            Map = new Tile[MapWidth, MapHeight];
+            MyUnitsTiles = new List<Tile>();
+            TeamManager = new TeamManager();
+
+            for (int i = 0; i < MapHeight; i++)
+            {
+                for (int j = 0; j < MapWidth; j++)
+                {
+                    inputs = Console.ReadLine().Split(' ');
+                    var scrapAmount = int.Parse(inputs[0]);
+                    if (scrapAmount > 0)
+                    {
+                        var tile = new Tile(j, i,
+                            scrapAmount,
+                            int.Parse(inputs[1]),
+                            int.Parse(inputs[2]),
+                            int.Parse(inputs[3]) == 1,
+                            int.Parse(inputs[4]) == 1,
+                            int.Parse(inputs[5]) == 1,
+                            int.Parse(inputs[6]) == 1);
+                        Map[j, i] = tile;
+
+                        if (tile.Owner == Constants.ME && tile.Units > 0)
+                        {
+                            MyUnitsTiles.Add(tile);
+                        }
+                    }
+                }
+            }
+
+            if (Direction == CampPosition.INIT)
+            {
+                Direction = MyUnitsTiles[0].X < MapWidth / 2 ? CampPosition.LEFT : CampPosition.RIGHT;
+            }
+
+            TeamManager.AssignMembersToTeams(MyUnitsTiles);
+
+            TeamManager.MoveTeams();
+
+            logger.PublishOutput();
+        }
+    }
+}
+
+public class TeamManager
+{
+    public AttackTeam AttackTeam { get; private set; }
+    public DefenseTeam DefenseTeam { get; private set; }
+    public BaseTeam BaseTeam { get; private set; }
+    public List<Unit> FullTeam { get; }
+
+    public TeamManager()
+    {
+        AttackTeam = new AttackTeam();
+        DefenseTeam = new DefenseTeam();
+        BaseTeam = new BaseTeam();
+        FullTeam = new List<Unit>();
+    }
+
+    public void AssignMembersToTeams(List<Tile> unitTiles)
+    {
+        //TODO here is complex logic
+        foreach (var tile in unitTiles)
+        {
+            for (var i = 0; i < tile.Units; i++)
+            {
+                FullTeam.Add(DefenseTeam.AddNewMember(tile));
+            }
+        }
+    }
+
+    public void MoveTeams()
+    {
+        AttackTeam.MoveMembers();
+        DefenseTeam.MoveMembers();
+        BaseTeam.MoveMembers();
+    }
+}
+
+public static class Helper
+{
+    public static void TestRandomScrappableAmounts(Logger logger, Tile[,] map, int mapWidth, int mapHeight, int nbOfTests)
+    {
+        var rnd = new Random();
+        var i = 0;
+        while (i < nbOfTests)
+        {
+            var x = rnd.Next(mapWidth);
+            var y = rnd.Next(mapHeight);
+            if (map[x, y] != null)
+            {
+                logger.LogDebugMessage("Tile [" + x + "," + y + "]: Total: " + map[x, y].TotalScrappableAmount);
+                i++;
+            }
+        }
+    }
+
+    public static void LogTeamCompositions(Logger logger, TeamManager teamManager)
+    {
+        foreach (var unit in teamManager.FullTeam)
+        {
+            logger.LogDebugMessage(unit.ToString());
+        }
+    }
+}
+
+public interface IAI
+{
+    string GetAction();
+    void CalculateTarget();
+}
+
+public abstract class AI : IAI
+{
+    public string GetAction()
+    {
+        return Action.Move(1, Location.X, Location.Y, Target.X, Target.Y);
+    }
+
+    public abstract void CalculateTarget();
+
+    protected Tile Location { get; set; }
+    protected Tile Target { get; set; }
+    protected GameInstance GameInstance = GameInstance.Instance;
+}
+
+public class AIAttack : AI
+{
+    public AIAttack(Tile tile)
+    {
+        Location = tile;
+        CalculateTarget();
+    }
+
+    public override void CalculateTarget()
+    { }
+}
+
+public class AIDefense : AI
+{
+    public AIDefense(Tile tile)
+    {
+        Location = tile;
+        CalculateTarget();
+    }
+
+    public override void CalculateTarget()
+    {
+        Target = GameInstance.Map[GameInstance.MapWidth / 2, Location.Y];
+    }
+}
+
+public class AIBase : AI
+{
+    public AIBase(Tile tile)
+    {
+        Location = tile;
+        CalculateTarget();
+    }
+
+    public override void CalculateTarget()
+    {
+        if (GameInstance.Direction == CampPosition.LEFT)
+        {
+            Target = GameInstance.Map[0, 0];
+        }
+        else
+        {
+            Target = GameInstance.Map[GameInstance.MapWidth - 1, 0];
+        }
+    }
+}
+
+public class Unit
+{
+    public Tile Tile { get; }
+    public UnitTeam Team { get; }
+    IAI ai;
+
+    public Unit(UnitTeam team, Tile tile)
+    {
+        Tile = tile;
+
+        switch (team)
+        {
+            case UnitTeam.ATTACK:
+                Team = UnitTeam.ATTACK;
+                ai = new AIAttack(tile);
+                break;
+            case UnitTeam.DEFENSE:
+                Team = UnitTeam.DEFENSE;
+                ai = new AIDefense(tile);
+                break;
+            case UnitTeam.BASE:
+                Team = UnitTeam.BASE;
+                ai = new AIBase(tile);
+                break;
+        }
+    }
+
+    public string GetAction()
+    {
+        return ai.GetAction();
+    }
+
+    public override string ToString()
+    {
+        return "I am a member of " + Team + " located at [" + Tile.X + "," + Tile.Y + "]!";
+    }
+}
+
+public abstract class Team
+{
+    public List<Unit> Members { get; protected set; }
+    protected UnitTeam TeamType;
+    Logger logger = Logger.Instance;
+
+    public Unit AddNewMember(Tile tile)
+    {
+        var result = new Unit(TeamType, tile);
+        Members.Add(result);
+        return result;
+    }
+
+    public void MoveMembers()
+    {
+        foreach (var unit in Members)
+        {
+            logger.LogAction(unit.GetAction());
+        }
+    }
+}
+
+public class AttackTeam : Team
+{
+    public AttackTeam()
+    {
+        Members = new List<Unit>();
+        TeamType = UnitTeam.ATTACK;
+    }
+}
+
+public class DefenseTeam : Team
+{
+    public DefenseTeam()
+    {
+        Members = new List<Unit>();
+        TeamType = UnitTeam.DEFENSE;
+    }
+}
+
+public class BaseTeam : Team
+{
+    public BaseTeam()
+    {
+        Members = new List<Unit>();
+        TeamType = UnitTeam.BASE;
+    }
+}
+
+public static class Action
+{
+    public static string Move(int amount, int fromX, int fromY, int toX, int toY)
+    {
+        return Constants.MOVE + " " + amount + " " + fromX + " " + fromY + " " + toX + " " + toY;
+    }
+
+    public static string Build(int x, int y)
+    {
+        return Constants.BUILD + " " + x + " " + y;
+    }
+
+    public static string Spawn(int amount, int x, int y)
+    {
+        return Constants.SPAWN + " " + amount + " " + x + " " + y;
+    }
+
+    public static string Wait()
+    {
+        return Constants.WAIT;
+    }
+}
+
+public sealed class Logger
+{
+    StringBuilder Output;
+    private static Logger instance;
+
+    private Logger()
+    {
+        Output = new StringBuilder();
+    }
+
+    public static Logger Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+    }
+
+    public void LogAction(string action)
+    {
+        Output.Append(action + ";");
+    }
+
+    public void LogUIMessage(string message)
+    {
+        Output.Append("MESSAGE " + message + ";");
+    }
+
+    public void LogDebugMessage(string message)
+    {
+        Console.Error.WriteLine(message);
+    }
+
+    public void PublishOutput()
+    {
+        var result = Output.ToString();
+
+        if (result != "")
+        {
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine(Action.Wait());
+        }
+        Output.Clear();
+    }
+
+}
+
+public class Tile
+{
+    public Tile(int x, int y, int scrapAmount, int owner, int units, bool recycler, bool canBuild, bool canSpawn, bool inRangeOfRecycler)
+    {
+        X = x;
+        Y = y;
+        ScrapAmount = scrapAmount;
+        Owner = owner;
+        Units = units;
+        Recycler = recycler;
+        CanBuild = canBuild;
+        CanSpawn = canSpawn;
+        InRangeOfRecycler = inRangeOfRecycler;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+    public int ScrapAmount { get; }
+    public int Owner { get; }
+    public int Units { get; }
+    public bool Recycler { get; }
+    public bool CanBuild { get; }
+    public bool CanSpawn { get; }
+    public bool InRangeOfRecycler { get; }
+
+    public int TotalScrappableAmount
+    {
+        get
+        {
+            if (fTotalScrappableAmount == 0)
+            {
+                var gameInstance = GameInstance.Instance;
+                var map = gameInstance.Map;
+
+                if (X < gameInstance.MapWidth - 1)
+                {
+                    fTotalScrappableAmount += map[X + 1, Y]?.ScrapAmount ?? 0;
+                }
+                if (X > 0)
+                {
+                    fTotalScrappableAmount += map[X - 1, Y]?.ScrapAmount ?? 0;
+                }
+                if (Y < gameInstance.MapHeight - 1)
+                {
+                    fTotalScrappableAmount += map[X, Y + 1]?.ScrapAmount ?? 0;
+                }
+                if (Y > 0)
+                {
+                    fTotalScrappableAmount += map[X, Y - 1]?.ScrapAmount ?? 0;
+                }
+
+                fTotalScrappableAmount += ScrapAmount;
+            }
+            return fTotalScrappableAmount;
+        }
+    }
+    int fTotalScrappableAmount = 0;
+
+    public override string ToString()
+    {
+        return "x: " + X + ", y:" + Y;
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,113 @@
+﻿ This is a Codingame.com challenge.
+ As per requirements of the platform, all code must be in a single file.
+ 
+ Below are my notes and thoughts on how to tackle this challenge.
+
+ 
+ Questions:
+ 
+ - What is the global strategy and how do we achieve it ?
+    -> dominate with the number of tanks ?
+    -> build lots of factories to max out spawning capacity ?
+    -> divide in teams ? 
+        => one attack team that eliminates opponents, and one team that colonizes ? Or one that moves forward and one that covers the back ?
+!!!!!!!!=> maybe three teams: attack goes for the opponents tiles, defense tries to eradicate enemy tanks getting closer, and base team tries to cover all neutral tiles starting from our camp?
+
+ - What is our 3 teams strategy (draft)?
+    -> first, half our starting units go in base team and stay behind
+        => each unit starts covering each neutral tile from the back to middle
+    -> the other half goes into defense team and move straight for the middle area
+    -> when defense team reaches middle area, we start spawning new units between attack and defense
+        => we need to find the right balance between reinforcing defense numbers and start deploying attackers
+    -> attack units start moving forward towards (furthest?) enemy occupied tiles
+    -> defense team gets most of the spawned tanks. Two options:
+        => scenario 1: we first spread across the vertical axis and try to cover each tile, to block enemies first, and then we reinforce each tile until we reach X units per tile
+        => scenario 2: we first try to reinforce our units to X amount, to protect against strong enemy, and then we start spreading vertically
+            - I think I prefer scenario 1, as scenario will leave us wide open for a long time
+            - Furthermore, as soon as we have covered the vertical axis, we can start alternating (as new units cant move) between spawning and moving all units forward by one to improve score
+    -> in parrallel to all that, we need to build factories:
+        => either we play it safe and build the furthest back to make sure we dont block our path, but we lose potentially lots of territory
+        => or more risky, we build in front of the defense to limit the tiles lost, in the risk of blocking ourselves if not spread well
+        => or maybe we build behind the defense ? Then we can isolate part of our territory from the enemy at the risk of isolating ourselves if defense and attack teams lose
+            -This needs testing but first option might be easier and preferable
+
+ - Where do we move each tank ?
+    -> if in 3 teams:
+        => attack should go for enemy occupied tiles
+        => defense: 
+            - if not in middle zone yet, move towards middle and wait for enemy coming
+            - if in middle and no enemy, wait
+            - if enemy approaching and enough units, attack
+        => base should move to closest neutral tile and proceed from left to right (or vice versa)
+ 
+ - Where is the best location to spawn tank?
+    -> if in teams: forward for attack, back-middle for coloniz ? on another tank's tile for attack, on a different one for coloniz?
+    -> if 3 teams scenario:
+        => spawn for defense first, once reached the middle
+        => spawn for attack, from the front of the defense, once defense has reached middle
+        => for now, we don't spawn for base team. To be adjusted if not sufficient
+ 
+ - When is the best time to spawn a tank?
+    -> any time we have more than 10 scrap ? (const here) : build of factory happens first, so if we want to build factory and have only 10, factory will be built
+ 
+ - How many tanks should we spawn ?
+    -> I think we shouldn't have an upper limit for the number of tanks, the more the better, at least until the computation for each movement is fast enough
+    -> for the number of tanks spawned...maybe 1 each time for now
+
+ - How to balance the teams (in 3 teams scenario) ?
+    -> base team should probably be the smallest team. Maybe to start with half the original amount (2 so far) and keep it at that
+    -> from start, prioritize defense team, while moving to center
+
+ - Where is the best location to build a recycler?
+    -> the scrap value should be worth it on this tile and around (create const for that threshold, add property on Tile for scrap sum ?)
+    -> when a factory has finished recycling, we lose 5 tiles worth of score 
+        => if we build too many in our camp, our score will go down fast!
+        => the more forward the build, the better ?
+ 
+ - When is the best time to build a recycler?
+    -> whenever we have more than 10 scraps (const), we have a suitable tile (buildable and above threshold above), and we are below our target factories described below
+ 
+ - How many recycler should we build ?
+    -> if we have too many, we lose tiles; if we have too few we won't collect enough scrap to win
+    -> several options:
+        => have a const with a fixed value
+            :simplest
+        => have a const that is initialized at game start based on a certain parameter: probably the grid size-> the bigger the grid the higher number of recycler needed at any time
+            :easy
+        => have a variable updated each turn depending on certains factors...eg: if we have lots of scrap, we may not need as many factories, especially if we have a low score
+            we also may not want too many factories from the start, or we ll get stuck; so maybe the variable should account for the turn number
+            :hardest but smartest 
+ 
+ - How do we prioritize between recycler build or tank build ? When should we save scraps ?
+    -> I think we don't: if we want a factory, we build it, if we have enough left, we build 1 tank (for now)
+    -> if we start building several factories/tanks per turn, maybe we ll need a strategy for saving scraps
+
+Architecture:
+
+ DONE- class GameInstance that handles game loop, read input and keep track of global variables
+ DONE- static class Constants
+ - class Unit, contains the GetAction method, possess an AI of type IAI, instantiated in constructor when unit is being assigned to a team
+ - abstract class Team
+ - class AttackTeam : Team contains list of Unit
+ - class DefenseTeam : Team contains list of Unit
+ - class BaseTeam : Team contains list of Unit
+ DONE- class Tile with all details provided by input
+ DONE- global Map variable: 2 dimensional array of Tiles, with null for grass Tiles
+ - global list of Neutral Tiles
+ - global list of my Tiles
+ - global list of enemy Tiles
+ - const (for now) IdealFactories
+ - const ScrapsToBuild = 10
+ - const DesiredScrapsToBuild
+ - const DesiredScrapsToSpawn
+ - interface IAI, contains GetTarget
+ - class AIAttack : IAI
+ - class AIDefense : IAI
+ - class AIBase : IAI
+ DONE- singleton class Logger, that will handle logging and output
+
+Refactoring/Improvements TODO:
+
+ DONE- Store commands for each turn into a StringBuilder (probably) and just append with ; at the end
+ DONE- Create a GameInstance class instantiated in Main, to get out of static context
+ - Determine and store if we start from left side or right side: if any tank.X < Gridwidth / 2 -> we are on the left, otherwise we are on the right
